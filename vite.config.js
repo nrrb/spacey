4a01eb1562db012611053141bf9e9aeb66a5d7d1
Nicholas Bennett <nicholasbennett555@gmail.com>
2025-04-27 02:09:24 -0400

    Clean up build process.

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
   plugins: [vue()],
-  base: '/spacey/',
-  build: {
-    outDir: 'docs'
-  }
-})
+  base: mode === 'production' ? '/spacey/' : '/'
+}))
