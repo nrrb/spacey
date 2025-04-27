e02272bdd92a9d453f7d785c9700c0f2623b2f19
Nicholas Bennett <nicholasbennett555@gmail.com>
2025-04-27 02:52:09 -0400

    Fix scaling issue on mobile.

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-overlay absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white z-10 max-w-3xl w-full p-8">
+  <div class="text-overlay absolute md:top-1/2 top-8 left-1/2 transform -translate-x-1/2 md:-translate-y-1/2 text-white z-10 max-w-3xl w-full p-8">
     <div class="flex flex-col md:flex-row items-center md:items-start space-y-6 md:space-y-0 md:space-x-6">
       <img
         src="/images/me.jpg"
